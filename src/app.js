9d4c908ab5e6d795afab19c00f6178f026abe488
YashSingh00987 <mohitsingh00987@gmail.com>
2024-10-15 19:27:12 +0530

    added validation & sanity checkes

--- src/app.js
+++ src/app.js
@@ -16,36 +16,110 @@ app.post("/signup", async (req, res) => {
   }
 });
 
-app.get("/user", async (req, res) => {
+app.get("/user", async (req, res, next) => {
+  
+
+  if(!req.body.email){
+    next();
+  }
   const userEmail = req.body.email;
 
-  //console.log(userEmail);
-
   const user = await User.findOne({ email: userEmail });
-
 
   try {
     if (!user) {
       res.status(404).send("User not found");
     } else {
-        res.send(user);
+      res.send(user);
     }
   } catch (err) {
     res.status(400).send("Something went wrong");
   }
 });
 
-app.get("/feed",async (req,res) => {
-  try{
+app.get("/user", async (req, res) => {
+  const id = req.body.userId;
+
+  try {
+    const user = await User.findById(id);
+
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong: " + err.message);
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
     const users = await User.find({});
-    res.send(users)
-
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong!!!");
   }
-  catch(err) {
-    res.status(400).send("Something went wrong!!!")
-  }
-})
+});
 
+app.delete("/user", async (req, res) => {
+  const id = req.body.userId;
+
+  try {   
+    const user = await User.findByIdAndDelete(id);
+    res.send("User deleted successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong!!!");
+  }
+});
+
+app.patch("/user/:userId", async (req, res) => {
+  const id = req.params?.userId;
+  const updatedData = req.body;
+
+  
+
+  try{
+
+    const ALLOWED_UPDATES = [
+      'password', 
+      'age', 
+      'gender', 
+      'photoUrl', 
+      'about', 
+      'skills'
+    ];
+
+    const isAllowedUpdate = Object.keys(updatedData).every(
+      (update) => ALLOWED_UPDATES.includes(update)
+    );
+
+
+    if(!isAllowedUpdate){
+      console.log("Update not allowed");
+      throw new Error('Update not allowed');
+    }
+
+    if(updatedData?.skills.length > 10){
+      throw new Error('Skills array cannot be more than 10');
+    }
+
+    const user = await User.findByIdAndUpdate(id, updatedData, {runValidators:true});
+    if(!user){
+      res.status(404).send("User not found");
+    }
+    else{
+      res.send("User updated successfully");
+    }
+  }
+  catch(err){
+    res.status(400).json({ error: err.message });
+  }
+}
+)
+
+
+  
 connectDB()
   .then(() => {
     console.log("Database Successfully Connected!!");
--- src/models/user.js
+++ src/models/user.js
@@ -2,23 +2,66 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     firstName:{
-        type:String
+        type:String,
+        required:true,
+        minLength:4,
+        maxLength:50
     },
     lastName:{
         type:String
     },
     email:{
-        type:String
+        type:String,
+        lowercase:true,
+        required:true,
+        unique:true,
+        trim:true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Invalid Email " + value)
+            }
+        }
     },
     password:{
-        type:String
+        type:String,
+        required:true,
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error("Invalid Password " + value)
+            }
+        }
+        
     },
     age:{
-        type:Number
+        type:Number,
+        min:18,
     },
     gender:{
-        type:String
+        type:String,
+        validate(value){
+            if(!['male','female','others'].includes(value)){
+                throw new Error("Invalid Gender " + value)
+            }
+        }
     },
+    photoUrl:{
+        type:String,
+        validate(value){
+            if(!validator.isURL(value)){
+                throw new Error("Invalid URL " + value)
+            }
+        }
+    },
+    about:{
+        type:String,
+        default:"Default Value"
+    },
+    skills:{
+        type:[String]
+    },
+    
+},{
+    timestamps:true
 })
 
 module.exports = mongoose.model('User',userSchema);
